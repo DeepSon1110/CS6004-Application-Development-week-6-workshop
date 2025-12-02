3b8ab3d75e84e5df86eaacc338a6e4d331052fc1
Dip_San <khadkasandip489@gmail.com>
2025-12-02 09:17:01 +0545

    Task 2

---
+++ Task 2/Program.cs
@@ -0,0 +1,35 @@
+﻿public  delegate int Calculate(int x, int y);
+public delegate double DiscountStrategy(double price);
+public class Program
+{
+    public static int Add(int a, int b) => a + b;
+    public static int Subtract(int a, int b) => a - b;
+
+    public static double FestivalDiscount(double price) => price - (price * 0.2);
+
+    public static double SeasonalDiscount(double price) => price - (price * 0.1);
+
+    public static double NoDiscount(double price) => price;
+
+
+    public static void Main()
+    {
+        Calculate cal = Add;
+        Console.WriteLine("Addition: " + cal(10, 5));
+
+        Calculate cal2 = Subtract;
+        Console.WriteLine("Subtraction: " + cal2(10, 5));
+
+        DiscountStrategy discount = FestivalDiscount;
+        Console.WriteLine("Price after Festival Discount on Rs. 100: Rs." + discount(100));
+
+        DiscountStrategy discount2 = SeasonalDiscount;
+        Console.WriteLine("Price after Seasonal Discount on RS. 100: Rs. " + discount2(100));
+
+        DiscountStrategy discount3 = NoDiscount;
+        Console.WriteLine("No Discount on $100: Rs." + discount3(100));
+
+
+    }
+}
+
